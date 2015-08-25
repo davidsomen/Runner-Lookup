af606f73e60bbc6c3f5716d591468509bccc2e77
David Somen <davidsomen@etech.net>
2015-08-25 19:28:49 +0100

    Initial Commit

---
+++ Runner Lookup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Runner Lookup
+//
+//  Created by David Somen on 25/08/2015.
+//  Copyright (c) 2015 David Somen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
